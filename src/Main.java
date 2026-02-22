2803068b91a951eeeac6b567966f1a7ae887ace1
anungood <anungood888@gmail.com>
2026-02-22 16:32:37 +0200

    Neuron Neural Network Test

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        /**
         //variable declaration
         int inputSize = 30 * 20;
         int[] hiddenLayers = {64};
         int outputNeurons = 20;
+
         //initializing MLP
         MLP network = new MLP(inputSize, hiddenLayers, outputNeurons);
         //sample testing the MLP, Layer and segmentation class
@@ -26,7 +28,7 @@ public class Main {
         for (double num : output) {
             System.out.println(num);
         }
-
+        //turning output into probabilities
         double[] probOutput = network.softmax(output);
         System.out.println("Probability of Output:");
         for (double num : probOutput) {
@@ -39,6 +41,30 @@ public class Main {
             sum += p;
         }
         System.out.println("Total sum: " + sum);
+        **/
 
+        //sample target neuron
+        Neuron neuron = new Neuron(2);
+        double[] inputs = {0.5, 0.8};
+        double target = 1.0;
+        double learningRate = 0.1;
+
+        //update weight by using the error for each iteration
+        for (int epoch = 0; epoch < 100000; epoch++) {
+            //forward prop for each neurons
+            double output = neuron.forward(inputs);
+            //calculating error
+            double error = output - target;
+            //update weights with the error
+            neuron.updateWeights(error, learningRate);
+            //print output
+            if (epoch % 100 == 0) {
+                System.out.println("Epoch " + epoch + " Output: " + output);
+            }
+        }
+
+        //output
+        double finalOutput = neuron.forward(inputs);
+        System.out.println("Final: " + finalOutput);
     }
 }
--- src/Neuron.java
+++ src/Neuron.java
@@ -8,6 +8,9 @@ public class Neuron {
     private double[] weights;
     private double bias;
     private Random rand = new Random();
+    //variables to save the last input and output z
+    private double[] previousInputs;
+    private double previousOutput;
 
     // constructor with the input size
     public Neuron(int inputSize) {
@@ -27,24 +30,60 @@ public class Neuron {
 
     }
 
-    //forward pass to calculate z= weights*inputs+ sum
+    //forward pass to calculate z= weights*inputs+ bias
     public double forward(double[] inputs) {
         System.out.println("Inside neuron class's forward method");
         double sum = 0.0;
+        //saving the inputs as it will be useful for backprop
+        previousInputs = inputs.clone();
 
+        //finding the sum of weights * inputs and bias
         for (int i= 0; i < weights.length; i++) {
             sum+= weights[i] * inputs[i];
         }
-
         sum+= bias;
-
-        return sigmoid(sum);
+        //saving the output as it will useful for backprop
+        previousOutput = sigmoid(sum);
+        return previousOutput;
     }
 
     // activation function: sigmoid(x) → 1 / (1 + e^-x)
     // make any input value between 0 and 1
     //makes it non-linear: output=σ(z)
     private double sigmoid(double x) {
+
         return 1.0 / (1.0 + Math.exp(-x));
+    }
+
+    //derivative of sigmoid function
+    public double sigmoidDerivative() {
+        return previousOutput * (1 - previousOutput);
+    }
+
+    //update weights after calculation
+    public void updateWeights(double error, double learningRate) {
+        //compute delta = error * sigmoid derivative
+        double delta = error * sigmoidDerivative();
+
+        //w_i = w_i - learningRate * delta * last input
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = weights[i] - learningRate * delta * previousInputs[i];
+        }
+
+        //update bias
+        bias -= learningRate * delta;
+    }
+
+    //getters and setters
+    public double[] getLastInputs() {
+        return previousInputs;
+    }
+
+    public double getLastOutput() {
+        return previousOutput;
+    }
+
+    public double[] getWeights() {
+        return weights;
     }
 }
